e79deb8bac89c92aed2ac54e45769024734c194a
Randy Beckford <bec16021@byui.edu>
2024-01-13 00:52:15 -0800

    Completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random  randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+        int guess = -1;
+
+        //I will use a do-while loop here
+        while (guess != magicNumber)
+        {
+
+            Console.Write("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("higher");
+            }
+             else if (magicNumber < guess)
+             {
+                Console.WriteLine("Lower");
+             }
+             else
+             {
+                Console.WriteLine("You guessed it!");
+             }
+        }
+
+        
     }
 }
