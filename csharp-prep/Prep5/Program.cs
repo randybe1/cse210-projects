945b617b3f3779bdc0bf4fcb7bbd2e653645ca37
Randy Beckford <bec16021@byui.edu>
2024-01-13 19:31:54 -0800

    Completed Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,48 @@ using System;
 
 class Program
 {
+    //Welcome to the program!
+    //Please enter your name: Brother Burton
+    //Please enter your favorite number: 42
+    //Brother Burton, the square of your number is 1764
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        
+            string userName = PromptUserName();
+            int userNumber = PromptUserNumber();
+
+            int squaredNumber = SquareNumber(userNumber);
+
+            DisplayResult(userName, squaredNumber);
+        
+    }
+     static void DisplayWelcomeMessage()    
+    {
+    Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+     static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
