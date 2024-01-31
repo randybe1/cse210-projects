ed4ee17d9cc251e4fd562e32030ebbd753ae8cc7
Randy Beckford <bec16021@byui.edu>
2024-01-31 00:25:51 -0800

    Completed learning Activity

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learning04
+{
+    public class WritingAssignment : Assignment
+    {
+        private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+        {
+                _title = title;
+        }
+
+    public string GetWritingInformation()
+    {
+       string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
+    }
+    }
+}
