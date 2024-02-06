dd8c4e0abd520d171b69269bd2e6b03892418bb2
Randy Beckford <bec16021@byui.edu>
2024-02-06 01:40:48 -0800

    1st attempt

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learning05
+{
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base (color)
+    {
+        _radius = radius;
+    }
+
+    // Notice the use of the override keyword here
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
+
+}
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Learning05
+{
+    public class Square : Shape
+{
+    private double _side;
+
+    public Square(string color, double side) : base (color)
+    {
+        _side = side;
+    }
+
+    // Notice the use of the override keyword here
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
+
+}
